a0a49a912e5f96954fca1085e386bbca9dcd327a
Jidraf Kimachia <raphjid170@gmail.com>
2022-09-08 21:01:35 +0300

    added migrations

--- db/migrate/20220908170221_create_players.rb
+++ db/migrate/20220908170221_create_players.rb
@@ -1,4 +1,13 @@
 class CreatePlayers < ActiveRecord::Migration[6.1]
   def change
+    create_table :players do |t|
+      t.string :name
+      t.integer :age
+      t.string :position
+      t.string :team_stadium
+      t.integer :team_id
+      t.integer :coach_id
+    end
   end
 end
+
--- db/migrate/20220908170239_create_coaches.rb
+++ db/migrate/20220908170239_create_coaches.rb
@@ -1,4 +1,7 @@
 class CreateCoaches < ActiveRecord::Migration[6.1]
   def change
+    create_table :coaches do |t|
+      t.string :coach_name
+    end
   end
 end
