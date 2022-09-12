4284c7a4a146bf98dc717301932bc098311e0f44
Jidraf Kimachia <raphjid170@gmail.com>
2022-09-12 10:28:27 +0300

    changed player migration

--- db/migrate/20220908170221_create_players.rb
+++ db/migrate/20220908170221_create_players.rb
@@ -4,9 +4,7 @@ class CreatePlayers < ActiveRecord::Migration[6.1]
       t.string :name
       t.integer :age
       t.string :position
-      t.string :team_stadium
-      t.integer :team_id
-      t.integer :coach_id
+      
     end
   end
 end
